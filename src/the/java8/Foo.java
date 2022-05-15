9cac54853735aaee84247999cf2d15cb1ad828c4
zangzangs <kimjangh94@gmail.com>
2022-05-15 16:12:47 +0900

    자바에서 제공하는 함수형 인터페이스

--- src/the/java8/Foo.java
+++ src/the/java8/Foo.java
@@ -1,17 +1,21 @@
 package the.java8;
 
+import java.util.function.Function;
+
 public class Foo {
 
     public static void main(String[] args) {
         int baseNumber = 10;
 
-        RunSomething runSomething = number -> number + baseNumber;
+        Function<Integer, Integer> plus10 = number -> number + 10;
+        Function<Integer, Integer> multiply2 = number -> number * 2;
 
-
-        System.out.println(runSomething.doIt(1));
-        System.out.println(runSomething.doIt(2));
-        System.out.println(runSomething.doIt(3));
+        //compose: 함수 적용 전 처리
+        System.out.println(plus10.compose(multiply2).apply(20));
+        //andThen: 함수 적용 후 처리
+        System.out.println(plus10.andThen(multiply2).apply(20));
 
 
     }
+
 }
