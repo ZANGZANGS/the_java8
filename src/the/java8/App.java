9cc130ed778289e940c979c6d07bf03b4db1844d
zangzangs <kimjangh94@gmail.com>
2022-05-16 23:46:21 +0900

    자바8 API 기본 메서드와 스태틱 메서드

--- src/the/java8/App.java
+++ src/the/java8/App.java
@@ -1,12 +1,31 @@
 package the.java8;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+
 public class App {
     public static void main(String[] args) {
-        Poo poo = new DefaultPoo("ZangZangs");
+        List<String> name = new ArrayList<>();
+        name.add("keesun");
+        name.add("toby");
+        name.add("zzs");
+        name.add("foo");
 
-        poo.printName();
-        poo.printNameUpperCase();
+//        name.forEach(System.out::println);
+
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while (spliterator.tryAdvance(System.out::println));
+        System.out.println("==============");
+        while (spliterator1.tryAdvance(System.out::println));
+
+        System.out.println("==============");
+        Comparator.reverseOrder();
+        Comparator.naturalOrder();
 
 
     }
 }
+
