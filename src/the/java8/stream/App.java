66f7492404a6edb4438c9fb9fecc14521b033b6d
zangzangs <kimjangh94@gmail.com>
2022-05-17 22:35:07 +0900

    Stream 소개

---
+++ src/the/java8/stream/App.java
@@ -0,0 +1,30 @@
+package the.java8.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("keesun");
+        names.add("toby");
+        names.add("zzs");
+        names.add("foo");
+
+        List<String> stringStream = names.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+
+        List<String> stringParallelStream = names.parallelStream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+
+        names.forEach(System.out::println);
+
+
+    }
+}
+
