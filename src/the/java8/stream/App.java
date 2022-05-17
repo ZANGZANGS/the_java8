b99c42fbb1d9f5a5f62d45bc1d10bdf7728ce25d
zangzangs <kimjangh94@gmail.com>
2022-05-17 23:21:06 +0900

    Stream API

--- src/the/java8/stream/App.java
+++ src/the/java8/stream/App.java
@@ -1,28 +1,75 @@
 package the.java8.stream;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("keesun");
-        names.add("toby");
-        names.add("zzs");
-        names.add("foo");
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
 
-        List<String> stringStream = names.stream()
-                .map(String::toUpperCase)
+
+        System.out.println("=====================");
+        System.out.println("spring 으로 시작하는 수업");
+        springClasses.stream()
+                .filter(v -> v.getTitle().startsWith("spring"))
+                .forEach(v -> System.out.println(v.getTitle()));
+
+        System.out.println("=====================");
+        System.out.println("close 되지 않은 수업");
+        springClasses.stream()
+                .filter(Predicate.not(OnlineClass::isClosed))
+                .forEach(v -> System.out.println(v.getTitle()));
+
+        System.out.println("=====================");
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        springClasses.stream()
+                .map(OnlineClass::getTitle)
+                .forEach(System.out::println);
+
+
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
+
+        List<List<OnlineClass>> zzsEvents = new ArrayList<>();
+        zzsEvents.add(springClasses);
+        zzsEvents.add(javaClasses);
+
+        System.out.println("=====================");
+        System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
+        zzsEvents.stream().flatMap(Collection::stream)
+                        .forEach(v-> System.out.println(v.getId()));
+
+        System.out.println("=====================");
+        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
+        Stream.iterate(10, i -> i + 1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("=====================");
+        System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
+        boolean test = javaClasses.stream().noneMatch(v -> v.getTitle().contains("Test"));
+        System.out.println(test);
+
+        System.out.println("=====================");
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
+        List<String> spring = springClasses.stream()
+                .map(OnlineClass::getTitle)
+                .filter(v -> v.contains("spring"))
                 .collect(Collectors.toList());
 
-
-        List<String> stringParallelStream = names.parallelStream()
-                .map(String::toUpperCase)
-                .collect(Collectors.toList());
-
-
-        names.forEach(System.out::println);
+        spring.forEach(System.out::println);
 
 
     }
---
+++ src/the/java8/stream/OnlineClass.java
@@ -0,0 +1,26 @@
+package the.java8.stream;
+
+public class OnlineClass {
+
+    private int id;
+    private String title;
+    private boolean closed;
+
+    public OnlineClass(int id, String title, boolean closed){
+        this.id = id;
+        this.title = title;
+        this.closed = closed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+}
