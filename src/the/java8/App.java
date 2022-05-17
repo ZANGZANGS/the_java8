beb78049c9711966f51b2e10a299aa7fe397c74e
zangzangs <kimjangh94@gmail.com>
2022-05-17 22:34:57 +0900

    rollback

--- src/the/java8/App.java
+++ src/the/java8/App.java
@@ -1,28 +1,29 @@
 package the.java8;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Spliterator;
 
 public class App {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("keesun");
-        names.add("toby");
-        names.add("zzs");
-        names.add("foo");
+        List<String> name = new ArrayList<>();
+        name.add("keesun");
+        name.add("toby");
+        name.add("zzs");
+        name.add("foo");
 
-        List<String> stringStream = names.stream()
-                .map(String::toUpperCase)
-                .collect(Collectors.toList());
+//        name.forEach(System.out::println);
 
-
-        List<String> stringParallelStream = names.parallelStream()
-                .map(String::toUpperCase)
-                .collect(Collectors.toList());
-
-
-        names.forEach(System.out::println);
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while (spliterator.tryAdvance(System.out::println));
+        System.out.println("==============");
+        while (spliterator1.tryAdvance(System.out::println));
+        
+        System.out.println("==============");
+        Comparator.reverseOrder();
+        Comparator.naturalOrder();
 
 
     }
