6d50afaeb358b85ef1ad7f44ae14d2ef5f88ed7a
zangzangs <kimjangh94@gmail.com>
2022-05-23 23:08:47 +0900

    CompletableFuture - completableFuture1

--- src/the/java8/completableFuture/App.java
+++ src/the/java8/completableFuture/App.java
@@ -2,33 +2,21 @@ package the.java8.completableFuture;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.*;
 
 public class App {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-
-        Callable<String> hello = () -> {
-            Thread.sleep(2000L);
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(()-> {
+            System.out.println("Hello " + Thread.currentThread().getName());
             return "hello";
-        };
+        }).thenApply((s) -> {
+            System.out.println(Thread.currentThread().getName());
+            return s.toUpperCase();
+        });
 
-        Callable<String> java = () -> {
-            Thread.sleep(3000L);
-            return "java";
-        };
+        future.get();
 
-        Callable<String> zzs = () -> {
-            Thread.sleep(1000L);
-            return "zzs";
-        };
-
-        String s = executorService.invokeAny(Arrays.asList(hello, java, zzs));
-
-        System.out.println(s);
-
-
-        executorService.shutdown();
 
     }
 
